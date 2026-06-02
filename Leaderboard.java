1e8f2f6e14ddeea3c140f42c9602788b1ccf5a25
Sabrina Natasha <sabrina.natashann@gmail.com>
2026-06-02 23:54:15 +0800

    Create Leaderboard.java

---
+++ Leaderboard.java
@@ -0,0 +1,43 @@
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Leaderboard {
+
+    public void sortScores(List<String> records) {
+
+        Collections.sort(records,
+                new Comparator<String>() {
+
+                    @Override
+                    public int compare(String a, String b) {
+
+                        int scoreA =
+                                Integer.parseInt(a.split(",")[1]);
+
+                        int scoreB =
+                                Integer.parseInt(b.split(",")[1]);
+
+                        return Integer.compare(scoreB, scoreA);
+                    }
+                });
+    }
+
+    public String getMedal(int rank) {
+
+        switch(rank) {
+
+            case 1:
+                return "🥇";
+
+            case 2:
+                return "🥈";
+
+            case 3:
+                return "🥉";
+
+            default:
+                return "";
+        }
+    }
+}
