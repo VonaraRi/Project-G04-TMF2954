890e7f4ba31fc4a9d0ac150f173497e9a070654d
103617-Brian <103617@siswa.unimas.my>
2026-06-05 07:27:22 +0800

    Update tester information in CustomStorageException

--- CustomStorageException.java
+++ CustomStorageException.java
@@ -2,7 +2,7 @@
  * Custom exception handling for storage-related failures (CSV and DB).
  * Satisfies the requirement for custom, robust exception usage.
  * Creator: Rionnalyn 106148
- * Tester:
+ * Tester: Chan Ka Hou 103617
  */
 public class CustomStorageException extends Exception {
 
