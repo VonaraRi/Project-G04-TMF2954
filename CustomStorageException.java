6d7ca2271503f7a8b4d3358251ff12cc6aeac5f4
RosiePosie24 <rosfanida24@gmail.com>
2026-06-04 20:37:27 +0800

    Update CustomStorageException.java

--- CustomStorageException.java
+++ CustomStorageException.java
@@ -2,9 +2,10 @@
  * Custom exception handling for storage-related failures (CSV and DB).
  * Satisfies the requirement for custom, robust exception usage.
  * Creator: Rionnalyn
- * Tester: 
+ * Tester:
  */
 public class CustomStorageException extends Exception {
+
     public CustomStorageException(String message, Throwable cause) {
         super("Storage Error: " + message, cause);
     }
