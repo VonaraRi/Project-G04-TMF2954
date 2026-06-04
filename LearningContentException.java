f966e09b1163c8fe05eb58f6aa2019e8627c7e0b
VonaraRi <rionnalynrichard5@gmail.com>
2026-06-04 23:34:05 +0800

    Update LearningContentException.java

--- LearningContentException.java
+++ LearningContentException.java
@@ -2,7 +2,7 @@
  * Custom exception for Learning Module content errors.
  *
  * Creator: Chan Ka Hou 103617
- * Tester:
+ * Tester: Rionnalyn 106148
  */
 
 public class LearningContentException extends Exception {
