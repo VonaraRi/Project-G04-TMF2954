2ac55b46ef235ec9ce14181523a08dfc2972d3e5
RosiePosie24 <rosfanida24@gmail.com>
2026-06-04 21:34:52 +0800

    Update QuestionType.java

--- QuestionType.java
+++ QuestionType.java
@@ -1,7 +1,13 @@
 /**
+ * Enum Name: QuestionType
+ * Description: This enum defines the different types of questions used in
+ * the SDG 3 Quiz Application, such as multiple choice, true or false,
+ * fill in the blank, and short answer questions.
+ *
  * Creator: Rosfanida ak Benjamin 106171
  * Tester:
  */
+
 //enum is used to define the types of question in the quiz.
 //each question can only havw one type from this list.
 
