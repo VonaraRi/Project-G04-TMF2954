c7be0f018a942acd508716e39873758935dadb88
RosiePosie24 <rosfanida24@gmail.com>
2026-06-04 22:33:14 +0800

    Update tester information in GamificationException

--- GamificationException.java
+++ GamificationException.java
@@ -1,7 +1,7 @@
 /**
  * Custom Exception for Gamification Module
  * Creator: Sabrina Natasha (106199)
- * Tester:
+ * Tester:Rosfanida ak Benjamin 106171
  */
 
 public class GamificationException extends Exception {
