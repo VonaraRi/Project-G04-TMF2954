20fd53aa145d1684c479e822207f9fb3772303dd
Sabrina Natasha <sabrina.natashann@gmail.com>
2026-06-05 11:24:34 +0800

    Update QuestionType.java

--- QuestionType.java
+++ QuestionType.java
@@ -5,7 +5,7 @@
  * fill in the blank, and short answer questions.
  *
  * Creator: Rosfanida ak Benjamin 106171
- * Tester:
+ * Tester: Sabrina Natasha
  */
 
 //enum is used to define the types of question in the quiz.
