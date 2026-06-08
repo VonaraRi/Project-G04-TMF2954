1833808115e7cc38629f0bc50abd78e845670794
Sabrina Natasha <sabrina.natashann@gmail.com>
2026-06-09 07:54:39 +0800

    Added more comments for further clarity

--- Reward.java
+++ Reward.java
@@ -6,10 +6,18 @@
 
 public class Reward {
 
+    /** User's badge achievement. */
     private String badge;
+
+    /** Number of stars earned. */
     private int stars;
+
+    /** Motivational message shown to the user. */
     private String motivationalMessage;
 
+    /**
+     * Constructor to initialize reward details.
+     */
     public Reward(String badge,
                   int stars,
                   String motivationalMessage) {
@@ -19,18 +27,30 @@ public class Reward {
         this.motivationalMessage = motivationalMessage;
     }
 
+    /**
+     * Returns the badge earned.
+     */
     public String getBadge() {
         return badge;
     }
 
+    /**
+     * Returns the number of stars earned.
+     */
     public int getStars() {
         return stars;
     }
 
+    /**
+     * Returns the motivational message.
+     */
     public String getMotivationalMessage() {
         return motivationalMessage;
     }
 
+    /**
+     * Returns reward information as formatted text.
+     */
     @Override
     public String toString() {
 
