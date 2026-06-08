c54875cbc3e111236978f517182ba0b944523db0
103617-Brian <103617@siswa.unimas.my>
2026-06-08 15:29:07 +0800

    More Comments

--- LearningContentException.java
+++ LearningContentException.java
@@ -1,10 +1,18 @@
 /**
  * Custom exception for Learning Module content errors.
  *
+* This is used when the learning module has invalid content,
+ * for example if fewer than 10 learning screens are loaded.
+ *
+ * OOP concept:
+ * This demonstrates custom exception handling.
+ *
  * Creator: Chan Ka Hou 103617
  * Tester: Rionnalyn 106148
  */
 
+// Constructor receives a simple error message and adds a clear prefix.
+// This makes learning module errors easier to identify during debugging.
 public class LearningContentException extends Exception {
     public LearningContentException(String message) {
         super("Learning Content Error: " + message);
